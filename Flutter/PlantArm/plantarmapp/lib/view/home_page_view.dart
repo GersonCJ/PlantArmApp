1e1b07ce0905119bfdcbecb3417c976192b03717
Gerson Chadi Jr <gerson.chadi@global.ntt>
2022-09-07 01:22:24 -0300

    Email verification created

--- Flutter/PlantArm/plantarmapp/lib/view/home_page_view.dart
+++ Flutter/PlantArm/plantarmapp/lib/view/home_page_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:plantarmapp/firebase_options.dart';
@@ -22,6 +23,12 @@ class HomePage extends StatelessWidget {
           switch (snapshot.connectionState){
             
             case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false){
+                print('You are verified.');
+              } else {
+                print('You have to verify your e-mail.');
+              }
               return const Text('Done');
               default:
                 return const Text('Loading...');
