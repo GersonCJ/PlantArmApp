da87621b2f73ae8a6e3606c3d3594f9f02521b2e
Gerson Chadi Jr <gerson.chadi@global.ntt>
2022-11-10 00:29:15 +0100

    Prettier formatting for code cleaning and Adding Funcionality to MainUiSettings View

--- Flutter/PlantArm/plantarmapp/lib/dialog/dialog_ok_box.dart
+++ Flutter/PlantArm/plantarmapp/lib/dialog/dialog_ok_box.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
-void dialogOkBox(BuildContext context, String errorMessage) {
+void dialogOkBox(BuildContext context, String errorMessage,
+    {String typeBox = 'Error...'}) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
         backgroundColor: const Color.fromARGB(255, 0, 45, 0),
-        title: const Text("Error...", style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)),),
-        content: Text(errorMessage, style: const TextStyle(color: Color.fromARGB(255, 0, 255, 0)),),
+        title: Text(
+          typeBox,
+          style: const TextStyle(color: Color.fromARGB(255, 0, 255, 0)),
+        ),
+        content: Text(
+          errorMessage,
+          style: const TextStyle(color: Color.fromARGB(255, 0, 255, 0)),
+        ),
         actions: [
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            }, 
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 0, 45, 0)),
-              foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 0, 255, 0))),
-            child: const Text('OK', style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)))),        
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromARGB(255, 0, 45, 0)),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 0, 255, 0))),
+              child: const Text('OK',
+                  style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)))),
         ],
         elevation: 24.0,
       );
--- Flutter/PlantArm/plantarmapp/lib/view/login_view.dart
+++ Flutter/PlantArm/plantarmapp/lib/view/login_view.dart
@@ -14,7 +14,7 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
-    late final TextEditingController _password;
+  late final TextEditingController _password;
 
   @override
   void initState() {
@@ -33,70 +33,72 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HydroPlant'),
-        backgroundColor: const Color.fromARGB(255, 0, 45, 0),
-        foregroundColor: const Color.fromARGB(255, 0, 255, 0),
-      ),
-      body: 
-        Column(
-            children: [
-              TextField(
-                controller: _email,
-                enableSuggestions: false,
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                  hintStyle: TextStyle(color: Colors.black),
-                ),
+        appBar: AppBar(
+          title: const Text('HydroPlant'),
+          backgroundColor: const Color.fromARGB(255, 0, 45, 0),
+          foregroundColor: const Color.fromARGB(255, 0, 255, 0),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.black),
               ),
-              TextField(
-                controller: _password,
-                enableSuggestions: false,
-                autocorrect: false,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: TextStyle(color: Colors.black),
-                ),
+            ),
+            TextField(
+              controller: _password,
+              enableSuggestions: false,
+              autocorrect: false,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.black),
               ),
-              TextButton(onPressed: (() async {
-                try {
-                final username = _email.text;
-                final password = _password.text;
-                final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                  email: username,
-                  password: password);
-                }
-                on FirebaseAuthException catch (e){
-                  final messageError = e.code.toString();
-                  final treatedMessageError = toBeginningOfSentenceCase(messageError.split('-').join(' ')).toString();
-                  dialogOkBox(context, treatedMessageError);
-                  if (e.code == 'user-not-found'){
-                  final complement = '$treatedMessageError. Would you like to Register a new user ?';
-                  dialogPushCancelBox(context, complement, 'RegisterView');
-                } else {
-                  dialogOkBox(context, treatedMessageError);
-                }
-                }
-                final user = FirebaseAuth.instance.currentUser;
-                if (user?.emailVerified ?? false){
-                  if (!mounted) return;
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    '/mainui/',
-                    (route) => false
-              );
-                }else {
-                  if (!mounted) return;
-                  dialogVerificationBox(context);
-                }
-              }), style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 0, 45, 0)),
-                    foregroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 0, 255, 0))),
-                  child: 
-                    const Text('Log In')),
-            ],
-          ));
-          }
-        }
+            ),
+            TextButton(
+                onPressed: () async {
+                  try {
+                    final username = _email.text;
+                    final password = _password.text;
+                    final userCredential = await FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
+                            email: username, password: password);
+                  } on FirebaseAuthException catch (e) {
+                    final messageError = e.code.toString();
+                    final treatedMessageError = toBeginningOfSentenceCase(
+                            messageError.split('-').join(' '))
+                        .toString();
+                    dialogOkBox(context, treatedMessageError);
+                    if (e.code == 'user-not-found') {
+                      final complement =
+                          '$treatedMessageError. Would you like to Register a new user ?';
+                      dialogPushCancelBox(context, complement, 'RegisterView');
+                    } else {
+                      dialogOkBox(context, treatedMessageError);
+                    }
+                  }
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user?.emailVerified ?? false) {
+                    if (!mounted) return;
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/mainui/', (route) => false);
+                  } else {
+                    if (!mounted) return;
+                    dialogVerificationBox(context);
+                  }
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 0, 45, 0)),
+                    foregroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 0, 255, 0))),
+                child: const Text('Log In')),
+          ],
+        ));
+  }
+}
