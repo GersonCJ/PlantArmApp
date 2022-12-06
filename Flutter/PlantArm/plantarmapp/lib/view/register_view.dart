6f3d810e8943dcb6d9d2b7495a84ac3849fcde09
Gerson Chadi Jr <gerson.chadi@global.ntt>
2022-12-06 17:35:46 +0100

    Aprimoramento da RegisterView, em estilo e forma.

--- Flutter/PlantArm/plantarmapp/lib/view/register_view.dart
+++ Flutter/PlantArm/plantarmapp/lib/view/register_view.dart
@@ -5,7 +5,6 @@ import 'package:plantarmapp/dialog/dialog_push_cancel_box.dart';
 import 'package:intl/intl.dart' show toBeginningOfSentenceCase;
 import 'package:plantarmapp/dialog/dialog_verification_box.dart';
 
-
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
 
@@ -14,13 +13,11 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
-  
-    late final TextEditingController _email;
-    late final TextEditingController _password;
+  late final TextEditingController _email;
+  late final TextEditingController _password;
 
   @override
   void initState() {
-    // TODO: implement initState
     _email = TextEditingController();
     _password = TextEditingController();
     super.initState();
@@ -28,7 +25,6 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     _email.dispose();
     _password.dispose();
     super.dispose();
@@ -37,64 +33,108 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HydroPlant'),
+        appBar: AppBar(
+          title: const Text('HydroPlant'),
+          backgroundColor: const Color.fromARGB(255, 0, 45, 0),
+          foregroundColor: const Color.fromARGB(255, 0, 255, 0),
+        ),
         backgroundColor: const Color.fromARGB(255, 0, 45, 0),
-        foregroundColor: const Color.fromARGB(255, 0, 255, 0),
-      ),
-      body:
-        Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Colors.black),
-              ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(children: [
+            Column(
+              children: [
+                const SizedBox(height: 220, child: Text('')),
+                SizedBox(
+                  width: 360,
+                  child: TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    style:
+                        const TextStyle(color: Color.fromARGB(255, 0, 255, 0)),
+                    textAlign: TextAlign.left,
+                    decoration: const InputDecoration(
+                        fillColor: Color.fromARGB(255, 0, 255, 0),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color.fromARGB(255, 0, 255, 0)),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 0, 255, 0))),
+                        hintText: 'Email...',
+                        hintStyle:
+                            TextStyle(color: Color.fromARGB(255, 0, 255, 0))),
+                    cursorColor: const Color.fromARGB(255, 0, 255, 0),
+                  ),
+                ),
+                SizedBox(
+                  width: 360,
+                  child: TextField(
+                    controller: _password,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    style:
+                        const TextStyle(color: Color.fromARGB(255, 0, 255, 0)),
+                    textAlign: TextAlign.left,
+                    decoration: const InputDecoration(
+                        fillColor: Color.fromARGB(255, 0, 255, 0),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color.fromARGB(255, 0, 255, 0)),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 0, 255, 0))),
+                        hintText: 'Password...',
+                        hintStyle:
+                            TextStyle(color: Color.fromARGB(255, 0, 255, 0))),
+                    cursorColor: const Color.fromARGB(255, 0, 255, 0),
+                  ),
+                ),
+                const Divider(),
+                OutlinedButton(
+                    onPressed: (() async {
+                      try {
+                        final username = _email.text;
+                        final password = _password.text;
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: username, password: password);
+                      } on FirebaseAuthException catch (e) {
+                        final messageError = e.code.toString();
+                        final treatedMessageError = toBeginningOfSentenceCase(
+                                messageError.split('-').join(' '))
+                            .toString();
+                        if (e.code == 'email-already-in-use') {
+                          final complement =
+                              '$treatedMessageError. Would you like to go to Login ?';
+                          dialogPushCancelBox(context, complement, 'LoginView');
+                        } else {
+                          dialogOkBox(context, treatedMessageError);
+                        }
+                      }
+                      final user = FirebaseAuth.instance.currentUser;
+                      if (user?.emailVerified ?? false) {
+                      } else {
+                        if (!mounted) return;
+                        dialogVerificationBox(context);
+                      }
+                    }),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 0, 45, 0),
+                      foregroundColor: const Color.fromARGB(255, 0, 255, 0),
+                      side: const BorderSide(
+                        color: Color.fromARGB(255, 0, 255, 0),
+                      ),
+                    ),
+                    child: const Text('Register')),
+              ],
             ),
-            TextField(
-              controller: _password,
-              enableSuggestions: false,
-              autocorrect: false,
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Colors.black),
-              ),
-            ),
-            TextButton(onPressed: (() async {
-              try{
-                final username = _email.text;
-                final password = _password.text;
-                final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: username,
-                  password: password);
-              }
-              on FirebaseAuthException catch (e){
-                final messageError = e.code.toString();
-                final treatedMessageError = toBeginningOfSentenceCase(messageError.split('-').join(' ')).toString();
-                if (e.code == 'email-already-in-use'){
-                  final complement = '$treatedMessageError. Would you like to go to Login ?';
-                  dialogPushCancelBox(context, complement, 'LoginView');
-                } else {
-                  dialogOkBox(context, treatedMessageError);
-                }
-              }
-              final user = FirebaseAuth.instance.currentUser;
-                if (user?.emailVerified ?? false){
-                }else {
-                  if (!mounted) return;
-                  dialogVerificationBox(context);
-                }
-            }), style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 0, 45, 0)),
-                  foregroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 0, 255, 0))),
-                child: 
-                  const Text('Register')),
-          ],
+          ]),
         ));
-          }
-        }
+  }
+}
