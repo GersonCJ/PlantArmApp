0e7547f469719d790f9e7ba0468fb1384f5a8b54
Gerson Chadi Jr <gerson.chadi@global.ntt>
2022-12-14 16:52:50 +0100

    Replacing named routes for constant Routes.

--- Flutter/PlantArm/plantarmapp/lib/dialog/dialog_push_cancel_box.dart
+++ Flutter/PlantArm/plantarmapp/lib/dialog/dialog_push_cancel_box.dart
@@ -1,33 +1,47 @@
 import 'package:flutter/material.dart';
 
-void dialogPushCancelBox(BuildContext context, String errorMessage, String screen) {
+import '../constants/const.dart';
+
+void dialogPushCancelBox(
+    BuildContext context, String errorMessage, String screen) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
         backgroundColor: const Color.fromARGB(255, 0, 45, 0),
-        title: const Text("Alert !", style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)),),
-        content: Text(errorMessage, style: const TextStyle(color: Color.fromARGB(255, 0, 255, 0)),),
+        title: const Text(
+          "Alert !",
+          style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)),
+        ),
+        content: Text(
+          errorMessage,
+          style: const TextStyle(color: Color.fromARGB(255, 0, 255, 0)),
+        ),
         actions: [
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            }, 
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 0, 45, 0)),
-              foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 0, 255, 0))),
-            child: const Text('Cancel', style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)))),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromARGB(255, 0, 45, 0)),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 0, 255, 0))),
+              child: const Text('Cancel',
+                  style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)))),
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                (screen == 'LoginView') ? '/login/' : '/register/',
-                (route) => false
-              );
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 0, 45, 0)),
-              foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 0, 255, 0))),
-            child: const Text('Yes', style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)))),       
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    (screen == 'LoginView') ? loginRoute : registerRoute,
+                    (route) => false);
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromARGB(255, 0, 45, 0)),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 0, 255, 0))),
+              child: const Text('Yes',
+                  style: TextStyle(color: Color.fromARGB(255, 0, 255, 0)))),
         ],
         elevation: 24.0,
       );
